ee6cc01d09f189b9df0503a8b4251c22d0fd2017
Your Name <you@example.com>
2026-06-28 23:04:08 +0530

    changes-inji

--- admin/application/modules/blog/views/add_blog.php
+++ admin/application/modules/blog/views/add_blog.php
@@ -92,10 +92,7 @@
                     </div>
                 </div>
 
-                <?php
-                $blog_seo_row = null;
-                $this->load->view('blog/blog_seo_fields');
-                ?>
+                <?php $this->load->view('blog/blog_seo_fields', array('blog_seo_row' => null)); ?>
 
                 <div class="blog-form-card">
                     <div class="blog-form-card-head">
